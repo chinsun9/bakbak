466aff2de594b857191b8cf4e785a8684b9120a3
chinsun9 <chin_sung@naver.com>
2021-11-21 17:31:31 +0900

    refactor: type MapItem

--- src/replace.ts
+++ src/replace.ts
@@ -1,14 +1,14 @@
-type MapItem = [string, string][];
+type MapItem = [string, string];
 
 // 3글자 이상 특수한 경우
-const targetString3: MapItem = [
+const targetString3: MapItem[] = [
   ['대박쓰', '박쓰'],
   ['진짜루', '짜루'],
   ['진짜냐', '짜냐'],
 ];
 
 // 2글자
-const targetString2: MapItem = ['대박', '진짜', '존나'].map((str) => [
+const targetString2: MapItem[] = ['대박', '진짜', '존나'].map((str) => [
   str,
   str[1].repeat(2),
 ]);
